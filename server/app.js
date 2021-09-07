c07e824cd723c7c7157d723a7c426d96507258a4
Aaron <aaronwphillips@gmail.com>
2021-09-06 21:50:45 -0700

    api update

--- server/app.js
+++ server/app.js
@@ -12,24 +12,51 @@ const app = Express();
 const port = 5000;
 app.use(Express.json());
 app.use(CORS());
- 
+
 /** Routes */
-app.get('/', async (req, res) => {
-  res.send("Hello, Prisma and Express!");
+app.get('/', (req, res) => {
+  res.send({ "server_online": true });
 });
- 
-/** Server */
-app.listen(port, () => {
-  console.log("API listening on localhost:%i", port);
-});
+
 /** Get the full list of baked goods, return the object in our response */
 app.get('/products', async (req, res) => {
   try {
      const goods = await prisma.goods.findMany();
      res.status(200).send(goods);
   }
-  /** When Prisma fails to read from database, catch the error and send it as response */
-  catch (err) {
+   /** When Prisma fails to read from database, catch the error and send it as our response */
+   catch (err) {
+    res.status(500).send(err);
+ }
+});
+
+ /** Update an existing product */
+app.patch("/products/update", async (req, res) => {
+   try {
+     /** Use Prisma to write the data to our MySQL database */
+     await prisma.supplier.update({
+       where: {
+          id: req.body.id
+       },
+       data: {
+         name: req.body.name, 
+         type: req.body.type,
+         price: req.body.price,
+         description: req.body.description,
+         image: req.body.image,
+         stock: req.body.stock
+       }
+     });
+     /** Send a response telling the user our data has been written */
+     res.status(200).send({"message": "OK"});
+   }
+   /** When Prisma fails to write to database, catch the error and send it as response */
+   catch (err) {
      res.status(500).send(err);
-  }
+   }
+ });
+
+/** Server */
+app.listen(port, () => {
+console.log(`API listening on localhost: ${port}`);
 });
